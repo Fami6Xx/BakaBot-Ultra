2b307311d1ec4ad341389863ce560848cb9f5620
Fami6Xx <libojer@gmail.com>
2022-10-03 21:05:22 +0200

    feat: login is now functional

--- routes/public.js
+++ routes/public.js
@@ -6,6 +6,24 @@ const root = __dirname.replace('\\routes', "");
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendFile('public/mainPage/index.html', { root: root});
+})
+
+/* GET login page */
+router.get("/login", function (req, res, next) {
+  res.sendFile("public/login/login/index.html", {root: root});
+});
+
+/* POST login form */
+router.post("/login", function (req, res, next){
+  if(req.body.username == "root" && req.body.password == "root"){
+    req.session.loggedin = true;
+    req.session.username = req.body.username;
+    req.session.timeout = Date.now() + 1800000; // Half Hour = 1 800 000 ms
+
+    res.redirect("/private/")
+  }else{
+    res.sendStatus(401);
+  }
 });
 
 module.exports = router;
