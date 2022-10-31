3f9f8cfdebdbea50dd564d04185269d9f1fcfd22
Fami6Xx <libojer@gmail.com>
2022-11-01 00:02:52 +0100

    refactor: some small changes

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const root = __dirname.replace('\\routes', "");
-
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render("login");
+  res.render("index", {loggedIn: req.session.loggedIn, username: req.session.username, });
 })
 
 /* GET login page */
@@ -14,15 +12,15 @@ router.get("/login", function (req, res) {
 });
 
 /* POST login form */
-router.post("/login", function (req, res, next){
-  if(req.body.username == "root" && req.body.password == "root"){
+router.post("/login", function (req, res){
+  if(req.body.username === "root" && req.body.password === "root"){
     req.session.loggedin = true;
     req.session.username = req.body.username;
     req.session.timeout = Date.now() + 1800000; // Half Hour = 1 800 000 ms
 
     res.redirect("/private/")
   }else{
-    res.status(401).render("login", {userName: req.body.username, password: req.body.password, displayErrorMessage: true})
+    res.status(401).render("login", {username: req.body.username, password: req.body.password, displayErrorMessage: true})
   }
 });
 
