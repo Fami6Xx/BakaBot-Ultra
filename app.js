f6b79169003ecbbeaa3feaf36707a7f2bbb83d29
Fami6Xx <libojer@gmail.com>
2022-11-01 19:15:31 +0100

    chore: removed unused modules

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const bodyParser = require("body-parser");
 const session = require("express-session");
 
 const publicRouter = require('./routes/public');
